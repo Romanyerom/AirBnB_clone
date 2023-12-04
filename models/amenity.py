2e74c1b664a38cb497ebd0bd80a76fbc8ec42ea7
Romanyerom <Romanyerom@github.com>
2023-12-04 05:26:14 -0800

    correction

--- models/amenity.py
+++ models/amenity.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-"""Defines the Amenity class."""
+"""This module creates a Amenity class"""
+
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """Represents an amenity.
+    """Class for managing amenity objects"""
 
-    Attributes:
-        name (str): The name of the amenity.
-    """
-
-    def __init__(self):
-        self.name = ""
+    name = ""
